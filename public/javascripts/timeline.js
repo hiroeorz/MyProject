1a4c39cb3e6ef495ed2fc71e93a6300374032d5a
HIROE Shin <hiroe.orz@gmail.com>
2011-03-22 19:49:19 +0900

    use JSONP and iframe put method update

--- public/javascripts/timeline.js
+++ public/javascripts/timeline.js
@@ -14,77 +14,45 @@ var Timeline = function() {this.init();}
 		    this.update_cycle);
       },
 
-      homeTimeline: function(since_id) {
-	  var xhr = new XMLHttpRequest();
-	  var url = "http://localhost/statuses/home_timeline";
-
-	  if (since_id != undefined) {
-	      url += "?since_id=" + since_id
-	  }
-
-	  xhr.open("GET", url, true);
-	  xhr.withCredentials = true;
-	  xhr.onload = function() {
-	      var tweets = JSON.parse(this.responseText).reverse();
-	      var now_time_msec = new Date().getTime();
-
-	      for (var i = 0; i < tweets.length; i++) {
-
-		  var tweet = tweets[i];
-		  var user = tweet.user;
-
-		  var tweetTime = Date.parse(tweet.created_at);
-		  var time_offset_msg = 
-		      timeline.getTimeSinceMessage(now_time_msec, tweetTime);
-
-		  var user_name = document.createElement("div");
-		  $(user_name).addClass("tweet-username");
-		  user_name.innerHTML = user.name;
-		  $(user_name).append("<span class='timeline-time'>" + 
-				      time_offset_msg + 
-				     "</span>");
-
-		  var li = document.createElement("li");
-		  var img_div = document.createElement("div");
-		  $(img_div).addClass("img-div");
-		  
-		  var text_div = document.createElement("div");
-		  $(text_div).addClass("text-div");
-
-		  var clear_div = document.createElement("div");
-		  $(clear_div).css("clear", "both");
-		  
-		  var imgStr = "<img src='" + user.profile_image_url + "' />"
-		  $(img_div).append(imgStr);
-
-		  var p = document.createElement("p");
-		  p.innerHTML = tweet.text;
-		  $(text_div).append(p);
-
-		  var body_div = document.createElement("div");
-		  $(body_div).addClass("body-div");
-		  
-
-		  $(body_div).append(img_div);
-		  $(body_div).append(text_div);
-		  $(body_div).append(clear_div);
-		  $(li).append(body_div);
-		  $(li).append(user_name);
-		  $(li).append(clear_div);
-		  
-		  $("#tweets").prepend(clear_div)
-		  $("#tweets").prepend(li);
-
-		  if (timeline.latest_id == null ||
-		      parseInt(timeline.latest_id) < parseInt(tweet.id)) {
-		      timeline.latest_id = parseInt(tweet.id);
-		  }
-	      }
-	  }
-	  xhr.send();
+      update: function() {
+	  this.updateIframe();
       },
 
-      update: function() {
+      updateIframe: function() {
+	  var text = $("#tweet_text").attr("value");
+
+	  var d = document;
+	  var f = document.getElementById("tweet-form");
+	  var b = document.getElementById("tweet-button");
+
+	  // クロスドメインポスト用隠し iframe
+	  var i = d.createElement('iframe');
+	  i.style.display = 'none';
+	  d.body.appendChild(i);
+	  
+	  // レスポンスイベント取得用隠し iframe
+	  var i2 = d.createElement('iframe');
+	  i2.name = 'postresult';
+	  i2.style.display = 'none';
+	  d.body.appendChild(i2);
+	  
+	  // レスポンス時イベント登録
+	  i2.contentWindow.addEventListener('unload', function(e) {
+	      timeline.homeTimeline(this.latest_id);
+	      $("#tweet_text").val("");
+	  }, false);
+	  
+	  // クロスドメインへの POST メソッド送信
+	  var iDoc = i.contentWindow.document;
+	  iDoc.open();
+	  iDoc.write('<form method="POST" action="http://localhost/statuses/update" target="postresult">');
+	  iDoc.write('<input type="hidden" name="status" value="' + text + '" />');
+	  iDoc.write('</form>');
+	  iDoc.write('<script>window.onload = function(){document.forms[0].submit();}</script>');
+	  iDoc.close();	  
+      },
+
+      updateXHR2: function() {
 	  var text = $("#tweet_text").attr("value");
 	  var xhr = new XMLHttpRequest();
 	  var url = "http://localhost/statuses/update";
@@ -99,6 +67,97 @@ var Timeline = function() {this.init();}
 	  xhr.setRequestHeader('Content-Type', 
 			       'application/x-www-form-urlencoded');
 	  xhr.send("status=" + encodeURIComponent(text));
+      },
+
+      homeTimeline: function(since_id) {
+	  this.homeTimelineJSONP(since_id);
+      },
+
+      homeTimelineJSONP: function(since_id) {
+	  var url = "http://localhost/statuses/home_timeline";
+	  url += "?callback=?";
+
+	  if (since_id != undefined) {
+	      url += "&since_id=" + since_id
+	  }
+
+	  $.getJSON(url, function(jsonData) {
+	      timeline.setTweetDataToTimeline(jsonData);
+	  });
+      },
+
+      homeTimelineXHR2: function(since_id) {
+	  var xhr = new XMLHttpRequest();
+	  var url = "http://localhost/statuses/home_timeline";
+
+	  if (since_id != undefined) {
+	      url += "?since_id=" + since_id
+	  }
+
+	  xhr.open("GET", url, true);
+	  xhr.withCredentials = true;
+	  xhr.onload = function() {
+	      timeline.setTweetDataToTimeline(this.responseText);
+	  };
+	  xhr.send();
+      },
+
+      setTweetDataToTimeline: function(jsonData) {
+	  var tweets = JSON.parse(jsonData).reverse();
+	  var now_time_msec = new Date().getTime();
+
+	  for (var i = 0; i < tweets.length; i++) {
+
+	      var tweet = tweets[i];
+	      var user = tweet.user;
+	      
+	      var tweetTime = Date.parse(tweet.created_at);
+	      var time_offset_msg = 
+		  timeline.getTimeSinceMessage(now_time_msec, tweetTime);
+	      
+	      var user_name = document.createElement("div");
+	      $(user_name).addClass("tweet-username");
+	      user_name.innerHTML = user.name;
+	      $(user_name).append("<span class='timeline-time'>" + 
+				  time_offset_msg + 
+				  "</span>");
+	      
+	      var li = document.createElement("li");
+	      var img_div = document.createElement("div");
+	      $(img_div).addClass("img-div");
+	      
+	      var text_div = document.createElement("div");
+	      $(text_div).addClass("text-div");
+	      
+	      var clear_div = document.createElement("div");
+	      $(clear_div).css("clear", "both");
+	      
+	      var imgStr = "<img src='" + user.profile_image_url + "' />"
+	      $(img_div).append(imgStr);
+	      
+	      var p = document.createElement("p");
+	      p.innerHTML = tweet.text;
+	      $(text_div).append(p);
+	      
+	      var body_div = document.createElement("div");
+	      $(body_div).addClass("body-div");
+	      
+	      
+	      $(body_div).append(img_div);
+	      $(body_div).append(text_div);
+	      $(body_div).append(clear_div);
+	      $(li).append(body_div);
+	      $(li).append(user_name);
+	      $(li).append(clear_div);
+	      
+	      $("#tweets").prepend(clear_div)
+	      $("#tweets").prepend(li);
+	      
+	      if (timeline.latest_id == null ||
+		  parseInt(timeline.latest_id) < parseInt(tweet.id)) {
+		  timeline.latest_id = parseInt(tweet.id);
+	      }
+	  }
       },
 
       getTimeSinceMessage: function(fromTime, toTime) {
